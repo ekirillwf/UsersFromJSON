657a585fe0d1cc56dd3ee079cea228a5b9a861cf
Kirill Elsufev <eysmen@mail.ru>
2019-10-15 16:08:52 +0700

    add initial
    add initial files

---
+++ UsersFromJSON/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  UsersFromJSON
+//
+//  Created by Кирилл Елсуфьев on 15.10.2019.
+//  Copyright © 2019 Кирилл Елсуфьев. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Test")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
